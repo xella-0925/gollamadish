37131c9dc85cd5965be0a69c63b3c1886c085e1c
xella-0925 <124006971+xella-0925@users.noreply.github.com>
2023-03-14 15:40:57 +0800

    Update activity_2.py

--- pages/activity_2.py
+++ pages/activity_2.py
@@ -1,25 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-two_d_arr = np.array([[1, 0, 1]
+_arr_2d = np.array([[1, 0, 1]
                    , [0, 0, 0]
                    , [1, 0, 1]])
 
-def fill(x, y, color):
+def change(x, y, color):
+    '''
+    Changes a specific coordinate to a hue of preset color
+
+    Parameters
+    ----------
+    x : int
+        x coordinate
+    y : int
+        y coordinate
+    color : int
+        hue of color
+    '''
     
     fig = plt.figure()
-    for i in range(len(two_d_arr)):
-        for j in range(len(two_d_arr)):
-            two_d_arr[x][y] = color
+    for i in range(len(_arr_2d)):
+        for j in range(len(_arr_2d)):
+            _arr_2d[x][y] = color
 
-    img = plt.imshow(two_d_arr, cmap='rainbow', interpolation='none')
+    img = plt.imshow(_arr_2d, cmap='rainbow', interpolation='none')
     img.set_clim([0, 100])
     plt.colorbar()
     
     return fig
 
 def main():
-    fill(2, 1, 85)
+    change(2, 1, 85)
     
 if __name__=="__main__":
     main()
