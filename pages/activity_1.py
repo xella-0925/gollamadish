f32c9682882fa23f0988d7c33411d3f61adcbc27
xella-0925 <124006971+xella-0925@users.noreply.github.com>
2023-04-25 19:28:24 +0800

    Update activity_1.py

--- pages/activity_1.py
+++ pages/activity_1.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
+#st.set_option ('browser.gatherUsageStats' , False)
+
+
+
+
 def DDALine(x1, y1, x2, y2, color):
     fig = plt.figure()
 
@@ -106,6 +112,22 @@ def main():
     plt.show()
     bresenham(x, y, x_end, y_end, color)
     midpoint(x, y, x_end, y_end, color)
+    
+        st.title("Midterm Exam in CCS221")
+
+    st.sidebar.header("Line Algorithm Parameters")
+    _act1_x0, _act1_y0, _act1_x1, _act1_y1 = st.sidebar.slider('Starting X', 1, 100), \
+                     st.sidebar.slider('Starting Y', 1, 100), \
+                     st.sidebar.slider('Ending X', 1, 100, 10), \
+                     st.sidebar.slider('Ending Y', 1, 100, 10)
+    
+    st.header("Activity 1")
+    st.subheader("DDA Line Algorithm")
+    st.pyplot(act1.DDALine(_act1_x0, _act1_y0, _act1_x1, _act1_y1, 'ro'))
+    st.subheader("Bresenham's Line Algorithm")
+    st.pyplot(act1.bresenham(_act1_x0, _act1_y0, _act1_x1, _act1_y1, 'ro'))
+    st.subheader("Midpoint Line Algorithm")
+    st.pyplot(act1.midpoint(_act1_x0, _act1_y0, _act1_x1, _act1_y1, 'ro'))
 
 if __name__ == "__main__":
     main()
