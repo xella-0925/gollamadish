f01ada36d2ea6a6dd0c1bb23c13b3f905edc3b6c
xella-0925 <124006971+xella-0925@users.noreply.github.com>
2023-05-07 20:23:02 +0800

    Update activity_3.py

--- pages/activity_3.py
+++ pages/activity_3.py
@@ -91,6 +91,7 @@ if uploaded_file is not None:
                                                             "Shear (Y)"])
 
     if option == "Original Image":
+        st.subheader("Original Image")
         st.image(img_, use_column_width=True)
 
     elif option == "Translation":
@@ -98,6 +99,7 @@ if uploaded_file is not None:
         y = st.sidebar.slider("Vertical Shift", -200, 200, 50)
         result = translate(img_, rows, cols, x, y)
         plt_grph(result)
+        st.subheader("Translated Image")
         st.pyplot()
 
     elif option == "Scaling":
@@ -105,28 +107,33 @@ if uploaded_file is not None:
         y = st.sidebar.slider("Vertical Scaling Factor", 0.1, 5.0, 1.8, 0.1)
         result = scaling(img_, rows, cols, x, y)
         plt_grph(result)
+        st.subheader("Scaled Image")
         st.pyplot()
 
     elif option == "Rotation":
         angle = st.sidebar.slider("Angle of Rotation", -180, 180, 10)
         result = rotate(img_, rows, cols, angle)
         plt_grph(result)
+        st.subheader("Rotated Image")
         st.pyplot()
 
     elif option == "Flip":
         axis = st.sidebar.slider("Flip Axis", 0, 1, 1)
         result = flip(img_, axis)
         plt_grph(result)
+        st.subheader("Flipped Image")
         st.pyplot()
 
     elif option == "Shear (X)":
         factor = st.sidebar.slider("Shear Factor", -1.0, 1.0, 0.2, 0.01)
         result = shear_x(img_, rows, cols, factor)
         plt_grph(result)
+        st.subheader("Sheared(X) Image")
         st.pyplot()
 
     elif option == "Shear (Y)":
         factor = st.sidebar.slider("Shear Factor", -1.0, 1.0, 0.2, 0.01)
         result = shear_y(img_, rows, cols, factor)
         plt_grph(result)
+        st.subheader("Sheared(Y) Image")
         st.pyplot()
