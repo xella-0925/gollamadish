a0156c548e5df5c1aa5b1827a7af1631daf25c66
xella-0925 <124006971+xella-0925@users.noreply.github.com>
2023-05-07 19:49:22 +0800

    Update aika.py

--- pages/aika.py
+++ pages/aika.py
@@ -10,25 +10,22 @@ import tensorflow as tf
 tf.compat.v1.disable_eager_execution()
 
 
-def transform_obj(points, transform_type, angle, translation):
-    if transform_type == "Rotation":
-        rotation_matrix = tf.stack([
-            [tf.cos(angle), tf.sin(angle), 0],
-            [-tf.sin(angle), tf.cos(angle), 0],
-            [0, 0, 1]
-        ])
-        transformed_object = tf.matmul(tf.cast(points, tf.float32), tf.cast(rotation_matrix, tf.float32))
-    elif transform_type == "Translation":
-        translation_matrix = tf.stack([
-            [1, 0, translation[0]],
-            [0, 1, translation[1]],
-            [0, 0, 1]
-        ])
-        transformed_object = tf.matmul(tf.cast(points, tf.float32), tf.cast(translation_matrix, tf.float32))
-    else:
-        transformed_object = points
+def rotate_obj(points, angle):
+    rotation_matrix = tf.stack([
+        [tf.cos(angle), tf.sin(angle), 0],
+        [-tf.sin(angle), tf.cos(angle), 0],
+        [0, 0, 1]
+    ])
+    rotate_object = tf.matmul(tf.cast(points, tf.float32), tf.cast(rotation_matrix, tf.float32))
 
-    return transformed_object
+    return rotate_object
+
+
+def translate_obj(points, translation):
+    translation_vector = tf.constant(translation, dtype=tf.float32)
+    translated_object = tf.add(tf.cast(points, tf.float32), translation_vector)
+
+    return translated_object
 
 
 def _plt_basic_object_(points):
@@ -66,6 +63,7 @@ def _rectangle_(bottom_lower=(0, 0, 0), side_length=5, length=-4):
 
 # RIGHT TRIANGLE
 def _right_tri_(bottom_lower=(0, 0, 0), side_length=3):
+    
     bottom_lower = np.array(bottom_lower)
 
     points = np.vstack([
@@ -78,7 +76,6 @@ def _right_tri_(bottom_lower=(0, 0, 0), side_length=3):
         bottom_lower + [side_length, 0, 0],
         bottom_lower + [side_length, 0, 0],
         bottom_lower + [0, 0, side_length],
-
         bottom_lower,
     ])
 
@@ -93,7 +90,7 @@ def _tri_prism_(bottom_lower=(0, 0, 0), side_length=5, side=4, two=2):
         bottom_lower,
         bottom_lower + [0, side, 0],
         bottom_lower + [side, side, 0],  # bottom left back
-        bottom_lower + [side, 0, 0],  # bottom right back
+        bottom_lower + [side, 0, 0], #bottom right back
         bottom_lower + [two, side, side_length],
         bottom_lower + [two, side, side_length],
         bottom_lower + [two, 0, side_length],
@@ -109,18 +106,12 @@ def main():
     st.sidebar.header("Image Transformations")
     object_types = ["Rectangle", "Right Triangle", "Triangular Prism"]
     object_type = st.sidebar.selectbox("Select Object Type", object_types)
-    transform_types = ["Rotation", "Translation"]
-    transform_type = st.sidebar.selectbox("Select Transform Type", transform_types)
 
-    if transform_type == "Rotation":
-        angle = st.sidebar.slider("Rotation Angle", -180.0, 180.0, step=1.0)
-        translation = None
-    elif transform_type == "Translation":
-        angle = None
-        translation_x = st.sidebar.slider("Translation X", -5.0, 5.0, step=0.1)
-        translation_y = st.sidebar.slider("Translation Y", -5.0, 5.0, step=0.1)
-        translation_z = st.sidebar.slider("Translation Z", -5.0, 5.0, step=0.1)
-        translation = [translation_x, translation_y, translation_z]
+    transformation_types = ["Rotate", "Translate"]
+    transformation_type = st.sidebar.selectbox("Select Transformation Type", transformation_types)
+
+    angle = st.sidebar.slider("Rotation Angle", -180.0, 180.0, step=1.0)
+    translation = st.sidebar.slider("Translation", -5.0, 5.0, step=0.1, value=(0.0, 0.0, 0.0))
 
     if object_type == "Rectangle":
         init_object = _rectangle_(side_length=5, length=-4)
@@ -132,7 +123,10 @@ def main():
     points = tf.constant(init_object, dtype=tf.float32)
 
     with tf.compat.v1.Session() as session:
-        transformed_object = session.run(transform_obj(points, transform_type, np.radians(angle), translation))
+        if transformation_type == "Rotate":
+            transformed_object = session.run(rotate_obj(points, np.radians(angle)))
+        elif transformation_type == "Translate":
+            transformed_object = session.run(translate_obj(points, translation))
 
     _plt_basic_object_(transformed_object)
     st.pyplot(plt)
@@ -140,4 +134,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
