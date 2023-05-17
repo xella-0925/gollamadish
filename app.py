62ba60de1d195d317964bff3f7b6cfc3a5695469
xella-0925 <124006971+xella-0925@users.noreply.github.com>
2023-05-17 15:56:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import cv2
+
+st.title("MIDTERM TEST in COMPUTER GRAPHICS AND VISUAL COMPUTING (CC 221)")
+st.subheader("Submitted by:")
+st.subheader("ANGELIKA MARIE NAVA")
+st.subheader("MARIANE FAITH TORREVERDE")
+st.subheader("REYCEL B. SARMIENTO")
+st.subheader("BSCS 1-A")
+
