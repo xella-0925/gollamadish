5eed1d4411e8e5fbfb508a91afb0ea7350311aff
xella-0925 <124006971+xella-0925@users.noreply.github.com>
2023-05-17 16:07:47 +0800

    Update quiz 1 - image translation.py

--- pages/quiz 1 - image translation.py
+++ pages/quiz 1 - image translation.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import streamlit as st
 
+# Defines the translation matrix
 def translate(img_, rows, cols, Bx, By):
     translated_img_ = np.float32(([1, 0, Bx],
                                  [0, 1, By],
@@ -20,11 +21,12 @@ st.sidebar.header("Image Translation (Old - New)")
 uploaded_file = st.sidebar.file_uploader("Choose an image", type=['jpg', 'jpeg', 'png'])
 
 if uploaded_file is not None:
-    # Read image
+    # Reads and process the uploaded image
     img_ = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
     img_ = cv2.cvtColor(img_, cv2.COLOR_BGR2RGB)
     rows, cols, dims = img_.shape
 
+    # Gets user input for the translation parameters
     Bx_old = st.sidebar.number_input("Bx_old:", min_value=0, max_value=cols-1, step=1, value=0)
     By_old = st.sidebar.number_input("By_old:", min_value=0, max_value=rows-1, step=1, value=0)
 
@@ -33,14 +35,19 @@ if uploaded_file is not None:
 
     no_of_imgs = st.sidebar.slider("Number of Images:", min_value=1, max_value=10, value=1, step=1)
 
+    # Initialize the new translation coordinates
     Bx_new = Bx_old
     By_new = By_old
-
+    
+    # Displays the original image
     st.image(img_, caption="Original Image", use_column_width=True)
-
+    
+    # Performs translation and display new images
     for no_of_tests in range(no_of_imgs):
+       # Translates the image using the specified parameters
        new_img = translate(img_, rows, cols, Bx_new, By_new)
+    # Displays the new image
        st.image(new_img, caption=f"New Image {no_of_tests+1}", use_column_width=True)
-
+       # Updates the translation coordinates for the next iteration
        Bx_new += Tx
        By_new += Ty
